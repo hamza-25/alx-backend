38f18cd54f68a0e654669cb54eab2d06909c8b5d
hamza-25 <me677499@gmail.com>
2024-03-28 18:03:57 +0000

    paginate items

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""Define Pagination Module
+"""
+import csv
+import math
+from typing import List
+
+
+def index_range(page: int, page_size: int) -> tuple[int, int]:
+    """function named index_range that
+    takes two integer arguments page and page_size
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    index = (start_index, end_index)
+    return index
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """method named get_page that takes two integer arguments
+        Return paginate items
+        """
+        assert isinstance(page, int)
+        assert isinstance(page_size, int)
+        assert page > 0
+        assert page_size > 0
+
+        data_set = self.dataset()
+        total_pages = (len(data_set) + page_size - 1) // page_size
+        start, end = index_range(page, page_size)
+        if 1 <= page <= total_pages:
+            page_data = data_set[start:end]
+        else:
+            return []
+        return page_data
