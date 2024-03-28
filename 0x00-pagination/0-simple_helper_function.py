18cd58a38061b8bf08fc587e314f61eb12bc0d53
hamza-25 <me677499@gmail.com>
2024-03-28 15:18:03 +0000

    index function

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Define Pagination Module
+"""
+
+
+def index_range(page: int, page_size: int) -> tuple[int, int]:
+    """function named index_range that
+    takes two integer arguments page and page_size
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    index = (start_index, end_index)
+    return index
