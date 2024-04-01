397a48116f12254a100fcd57a6e8f006cfe45370
hamza-25 <me677499@gmail.com>
2024-04-01 23:41:12 +0000

    fix

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -40,7 +40,7 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: Optional[int] = None,
+    def get_hyper_index(self, index: int = None,
                         page_size: int = 10) -> Dict:
         """Deletion-resilient hypermedia pagination
         """
