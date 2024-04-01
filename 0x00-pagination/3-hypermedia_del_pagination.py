3940b3468d662e11ffc645c4f70826c31a253223
hamza-25 <me677499@gmail.com>
2024-04-01 22:52:39 +0000

    fix typing

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -5,7 +5,7 @@ Deletion-resilient hypermedia pagination
 
 import csv
 import math
-from typing import List, Optional
+from typing import List, Optional, Dict
 
 
 class Server:
@@ -29,7 +29,7 @@ class Server:
 
         return self.__dataset
 
-    def indexed_dataset(self) -> dict[int, List]:
+    def indexed_dataset(self) -> Dict[int, List]:
         """Dataset indexed by sorting position, starting at 0
         """
         if self.__indexed_dataset is None:
@@ -41,7 +41,7 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: Optional[int] = None,
-                        page_size: int = 10) -> dict:
+                        page_size: int = 10) -> Dict:
         """Deletion-resilient hypermedia pagination
         """
         dataset_length = len(self.dataset())
