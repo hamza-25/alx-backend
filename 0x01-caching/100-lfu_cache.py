a064f352d8696f6b7f1854f8cd5b4fe04f558b4e
hamza-25 <me677499@gmail.com>
2024-04-03 16:06:06 +0000

    100 lfu cache

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -13,20 +13,49 @@ class LFUCache(BaseCaching):
         """
         super().__init__()
         self.cache_data = OrderedDict()
+        self.usage_frequency = {}
 
     def put(self, key, item):
         """add key value pair to cache_data dict
         """
+        # if key is None or item is None:
+        #     return
+
+        # lfu_keys = OrderedDict()
+        # if key not in self.cache_data:
+        #     lfu_keys[key] = 0
+        #     if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+        #         list_v = [v for v in lfu_keys.values()]
+        #         min_v = min(list_v)
+        #         for k, v in lfu_keys.items():
+        #             if min_v == v:
+        #                 first_item_key = k
+        #                 break
+        #         # first_item_key, _ = list(self.cache_data.items())[0]
+        #         print(f'DISCARD: {first_item_key}')
+        #         del self.cache_data[first_item_key]
+        #     self.cache_data[key] = item
+        # else:
+        #     lfu_keys[key] += 1
+        #     self.cache_data[key] = item
         if key is None or item is None:
             return
-        if key not in self.cache_data:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                first_item_key, _ = list(self.cache_data.items())[0]
-                print(f'DISCARD: {first_item_key}')
-                del self.cache_data[first_item_key]
-            self.cache_data[key] = item
+
+        # Update usage frequency
+        if key in self.usage_frequency:
+            self.usage_frequency[key] += 1
         else:
-            self.cache_data[key] = item
+            self.usage_frequency[key] = 1
+
+        # Add or update item in cache_data
+        self.cache_data[key] = item
+
+        # If cache is full, discard least frequently used item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            min_key = min(self.usage_frequency, key=self.usage_frequency.get)
+            print(f"DISCARD: {min_key}")
+            del self.cache_data[min_key]
+            del self.usage_frequency[min_key]
 
     def get(self, key):
         """get key from cache_data dict
