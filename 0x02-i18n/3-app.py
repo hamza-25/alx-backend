0d46ea77d0ae3e40458f1e15ea2e01d2853b2745
hamza-25 <me677499@gmail.com>
2024-04-09 15:00:45 +0000

    update translate

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""app flask Module"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """class Config for translation"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """return best match with our supported languages"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def home() -> str:
+    """home route for app application"""
+    return render_template('3-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
