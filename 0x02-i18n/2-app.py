aad428b59b57ab6a6a3ef1528571306baef2f8dc
hamza-25 <me677499@gmail.com>
2024-04-09 14:15:22 +0000

    add docs

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -19,7 +19,7 @@ app.config.from_object(Config)
 
 @babel.localeselector
 def get_locale():
-    """"""
+    """return best match with our supported languages"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
