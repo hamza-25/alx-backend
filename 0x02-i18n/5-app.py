2dd486cac1ca61e5cab9e4a07dae7e07d25085ac
hamza-25 <me677499@gmail.com>
2024-04-09 16:47:36 +0000

    add translate and db user

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""app flask Module"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+from typing import Dict, Union
+
+
+class Config:
+    """class Config for translation"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+app.config.from_object(Config)
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """return best match with our supported languages"""
+    locale = request.args.get('locale')
+    if locale:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+def get_user() -> Union[Dict, None]:
+    """get user from query pram"""
+    id = request.args.get('login_as')
+    if id in users:
+        return users.get(int(id))
+    return None
+
+
+@app.before_request
+def before_request() -> None:
+    """before_reques assign user globaly"""
+    user = get_user()
+    g.user = user
+
+
+@app.route('/')
+def home() -> str:
+    """home route for app application"""
+    return render_template('3-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
