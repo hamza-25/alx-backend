90917503fec996c3668362388d70d94abaa419d1
hamza-25 <me677499@gmail.com>
2024-04-09 14:03:51 +0000

    change file html

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -17,7 +17,7 @@ app.config.from_object(Config)
 @app.route('/')
 def home() -> str:
     """home route for app application"""
-    return render_template('0-index.html')
+    return render_template('1-index.html')
 
 
 if __name__ == "__main__":
