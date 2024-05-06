56a4339d4873c8bdb250218d83a1ad85a3632ffc
hamza-25 <me677499@gmail.com>
2024-05-06 22:38:42 +0100

    add task 6 create process

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -3,8 +3,8 @@ const kue = require('kue');
 const queue = kue.createQueue({name: 'push_notification_code'});
 
 const job = queue.create('push_notification_code', {
-    phoneNumber: 'string',
-    message: 'string',
+    phoneNumber: '2126548745958',
+    message: 'platform',
 });
 
 job.on('enqueue', () => {
